86a6cea49416d1134d51289804c995a02a5c87d5
Tanvirahmed321110 <tanvirahmed321110@gmail.com>
2026-05-09 02:47:19 +0600

    first commit

---
+++ controllers/timesheets.py
@@ -0,0 +1,39 @@
+# from odoo import http
+# from odoo.http import request
+#
+# class XsellencePortal(http.Controller):
+#     @http.route('/timesheets',type='http',auth='public',website=True)
+#     def timesheet_f (self,**kw):
+#         return  request.render('xsellence_portal.timesheet_page',{
+#             'active_menu': 'timesheets',
+#             'breadcrumb': [
+#                 {'name': 'Dashboard', 'url': '/dashboard'},
+#                 {'name': 'Timesheets', 'url': False},
+#             ]
+#         })
+#
+#     # For Add Timesheet Page
+#     @http.route('/add_timesheet', type='http', auth='public', website=True)
+#     def add_timesheet_f(self, **kw):
+#
+#         source = kw.get('source')
+#         print(source)
+#
+#         if source == 'timesheets':
+#             breadcrumb_data = [
+#                 {'name': 'Dashboard', 'url': '/dashboard'},
+#                 {'name': 'Timesheets', 'url': '/timesheets'},
+#                 {'name': 'Add Timesheet', 'url': False},
+#             ]
+#
+#         else:
+#             breadcrumb_data = [
+#                 {'name': 'Dashboard', 'url': '/dashboard'},
+#                 {'name': 'Timesheets', 'url': False},
+#             ]
+#
+#         return request.render('xsellence_portal.add_timesheet_page', {
+#             'active_menu': 'add_timesheet',
+#             'breadcrumb': breadcrumb_data,
+#         })
+#
---
+++ models/student.py
@@ -0,0 +1,42 @@
+from odoo import models, fields, api
+
+from python.Lib.email.policy import default
+
+
+class SchoolStudent(models.Model):
+    _name = 'school.student'
+    _description = 'school student'
+    _rec_name = 'name'
+
+    name = fields.Char(string='Student Name', required=True)
+    roll = fields.Integer(string='Roll Number',required=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ], string='Gender')
+    admission_date = fields.Date(string='Admission Date',default=lambda self:fields.Date.today(),required=1)
+    class_name = fields.Selection([
+        ('six', 'Six'),
+        ('seven', 'Seven'),
+        ('eight', 'Eight'),
+        ('nine', 'Nine'),
+        ('ten', 'Ten'),
+    ], string='Class', required=True)
+
+    father_name = fields.Char(string="Father's Name")
+    mother_name = fields.Char(string="Mother's Name")
+    phone = fields.Char(string='Phone')
+    address = fields.Text(string='Address')
+    date_of_birth = fields.Date(string='Date of Birth')
+
+    is_even_roll = fields.Boolean(
+        strin='Even Roll',
+        compute = '_compute_is_even_roll',
+        store=True
+    )
+
+    # for  even roll
+    @api.depends('roll')
+    def _compute_is_even_roll(self):
+        for rec in self:
+            rec.is_even_roll = rec.roll % 2 ==0
